c8ecab5ebfec78501e22c609f59b9f93e99494b5
Lincoln Atkinson <LINCOLN.ATKINSON2478@STU.MTEC.EDU>
2023-10-13 09:20:39 -0600

    added card loading

--- app.js
+++ app.js
@@ -36,9 +36,14 @@ d.addEventListener('click', function() {
 let start = document.querySelector('.start');
 let home = document.querySelector('.home');
 let card = document.querySelector('.scoring');
+let cId = document.querySelector('.id');
+let table = document.getElementById('table');
 start.addEventListener('click', function() {
-    home.classList.add('hidden');
-    card.classList.remove('hidden');
+    if (cId.value !== ``) {
+        home.classList.add('hidden');
+        card.classList.remove('hidden');
+        buildCard();
+    }
 });
 let end = document.querySelector('.end');
 end.addEventListener('click', function() {
@@ -46,8 +51,128 @@ end.addEventListener('click', function() {
     card.classList.add('hidden');
 });
 
-fetch('http://uxcobra.com/golfapi/course11819.txt')
-    .then(response => response.text())
-    .then(data => {
-        const holeData = data;
-})
+function buildCard() {
+    if (cId.value === `11819`) {
+        fetch('http://uxcobra.com/golfapi/course11819.txt')
+            .then(response => response.json())
+            .then(data => {
+                const holes = data.data.holes;
+                const holeData = document.getElementById('holeData').getElementsByTagName('tbody')[0];
+
+                function populateTable() {
+                    const yardageRow = holeData.getElementsByTagName('tr')[0];
+                    const parRow = holeData.getElementsByTagName('tr')[1];
+                    const handicapRow = holeData.getElementsByTagName('tr')[2];
+
+                    yardageRow.innerHTML = '<th>Yardage</th>';
+                    parRow.innerHTML = '<th>Par</th>';
+                    handicapRow.innerHTML = '<th>Handicap</th>';
+
+                    let outSum = 0;
+                    let inSum = 0;
+
+                    holes.forEach(hole => {
+                        const teeBox = hole.teeBoxes.find(teeBox => teeBox.teeType === "men"); // Replace with the selected tee box
+                        if (teeBox) {
+                        yardageRow.insertCell().textContent = teeBox.yards;
+                        parRow.insertCell().textContent = teeBox.par;
+                        handicapRow.insertCell().textContent = teeBox.hcp;
+
+                        if (hole.hole <= 9) {
+                            outSum += teeBox.par;
+                        } else {
+                            inSum += teeBox.par;
+                        }
+                        }
+                    });
+
+                    const totalRow = holeData.getElementsByTagName('tr')[3];
+                    totalRow.innerHTML = `<th>Total</th><td colspan="9">${outSum}</td><td colspan="9">${inSum}</td><td colspan="6">${outSum + inSum}</td>`;
+                }
+                populateTable();
+            })
+    }
+    else if (cId.value === `18300`) {
+        fetch('http://uxcobra.com/golfapi/course18300.txt')
+            .then(response => response.text())
+            .then(data => {
+                const holes = data.data.holes;
+                const holeData = document.getElementById('holeData').getElementsByTagName('tbody')[0];
+
+                function populateTable() {
+                    const yardageRow = holeData.getElementsByTagName('tr')[0];
+                    const parRow = holeData.getElementsByTagName('tr')[1];
+                    const handicapRow = holeData.getElementsByTagName('tr')[2];
+
+                    yardageRow.innerHTML = '<th>Yardage</th>';
+                    parRow.innerHTML = '<th>Par</th>';
+                    handicapRow.innerHTML = '<th>Handicap</th>';
+
+                    let outSum = 0;
+                    let inSum = 0;
+
+                    holes.forEach(hole => {
+                        const teeBox = hole.teeBoxes.find(teeBox => teeBox.teeType === "men"); // Replace with the selected tee box
+                        if (teeBox) {
+                        yardageRow.insertCell().textContent = teeBox.yards;
+                        parRow.insertCell().textContent = teeBox.par;
+                        handicapRow.insertCell().textContent = teeBox.hcp;
+
+                        if (hole.hole <= 9) {
+                            outSum += teeBox.par;
+                        } else {
+                            inSum += teeBox.par;
+                        }
+                        }
+                    });
+
+                    const totalRow = holeData.getElementsByTagName('tr')[3];
+                    totalRow.innerHTML = `<th>Total</th><td colspan="9">${outSum}</td><td colspan="9">${inSum}</td><td colspan="6">${outSum + inSum}</td>`;
+                }
+                populateTable();
+        })
+    }
+    else if (cId.value === `19002`) {
+        fetch('http://uxcobra.com/golfapi/course19002.txt')
+            .then(response => response.text())
+            .then(data => {
+                holes = data.data.holes;
+                const holeData = document.getElementById('holeData').getElementsByTagName('tbody')[0];
+
+                function populateTable() {
+                    const yardageRow = holeData.getElementsByTagName('tr')[0];
+                    const parRow = holeData.getElementsByTagName('tr')[1];
+                    const handicapRow = holeData.getElementsByTagName('tr')[2];
+
+                    yardageRow.innerHTML = '<th>Yardage</th>';
+                    parRow.innerHTML = '<th>Par</th>';
+                    handicapRow.innerHTML = '<th>Handicap</th>';
+
+                    let outSum = 0;
+                    let inSum = 0;
+
+                    holes.forEach(hole => {
+                        const teeBox = hole.teeBoxes.find(teeBox => teeBox.teeType === "men"); // Replace with the selected tee box
+                        if (teeBox) {
+                        yardageRow.insertCell().textContent = teeBox.yards;
+                        parRow.insertCell().textContent = teeBox.par;
+                        handicapRow.insertCell().textContent = teeBox.hcp;
+
+                        if (hole.hole <= 9) {
+                            outSum += teeBox.par;
+                        } else {
+                            inSum += teeBox.par;
+                        }
+                        }
+                    });
+
+                    const totalRow = holeData.getElementsByTagName('tr')[3];
+                    totalRow.innerHTML = `<th>Total</th><td colspan="9">${outSum}</td><td colspan="9">${inSum}</td><td colspan="6">${outSum + inSum}</td>`;
+                }
+                populateTable();
+        })
+    }
+    else {
+        alert(`Error: No Course Found With The Current Id. Please Refresh The Page`);
+    }
+}
